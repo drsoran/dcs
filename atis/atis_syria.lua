3709fd015097b18698d38fa50a929a381df912be
Ronny Röhricht <ronny.roehricht@gmail.com>
2021-11-05 20:34:14 +0100

    feat: add SRS path env

--- atis/atis_syria.lua
+++ atis/atis_syria.lua
@@ -3,10 +3,16 @@ local atis_airports = {
 }
 
 local function StartATIS()
+    local srsPath = os.getenv("SRS_PATH");
+    if not srsPath then
+        env.error("Could not find environmant variable SRS_PATH, ATIS will not start.");
+        return;
+    end
+
     for i = 1, #atis_airports do
         local ap = atis_airports[i];
         local atis = ATIS:New(ap[1], ap[2]);
-        atis:SetSRS("d:\\DCS-SimpleRadio-Standalone", "female", "en-US");
+        atis:SetSRS(srsPath, "female", "en-US");
         atis:SetAltimeterQNH(false);
         atis:ReportZuluTimeOnly();
         atis:SetQueueUpdateTime(40);
